0d09d303f8ff0b0937e3a8821c099cf4d938e31e
Christian Parra <christian.parrita@gmail.com>
2025-06-26 21:30:05 -0700

    chore: added blowfish dep

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,42 @@
-// hello world
-
 #include <iostream>
+#include <string>
+
+#include <blowfish/blowfish.h>
+
+void test_blowfish()
+{
+    // Initialize Blowfish with a key
+    std::string key = "MySecretKey123";
+    Blowfish bf(key);
+
+    // Test data
+    uint32_t left = 0x12345678;
+    uint32_t right = 0x90ABCDEF;
+
+    // Make a copy for verification
+    uint32_t orig_left = left;
+    uint32_t orig_right = right;
+
+    // Encrypt
+    bf.encrypt(left, right);
+
+    // Decrypt
+    bf.decrypt(left, right);
+
+    // Verify
+    if (left == orig_left && right == orig_right)
+    {
+        std::cout << "Blowfish test passed!" << std::endl;
+    }
+    else
+    {
+        std::cout << "Blowfish test failed!" << std::endl;
+    }
+}
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    test_blowfish();
+
     return 0;
 }
