882159fe0e3c44fa56b13781f0a13e876bfcd178
Christian Parra <christian.parrita@gmail.com>
2025-07-12 20:52:33 -0700

    feat: MoveToLocation response packet

---
+++ src/game/packets/responses/move_to_location.cpp
@@ -0,0 +1,48 @@
+#include "move_to_location.hpp"
+#include <iostream>
+
+// Constructor
+MoveToLocation::MoveToLocation(const Player* player)
+    : m_objectId(player->getObjectId()),
+      m_x(player->getX()),
+      m_y(player->getY()),
+      m_z(player->getZ()),
+      m_xDst(player->getX()), // TODO: Implement destination coordinates in Player class
+      m_yDst(player->getY()), // TODO: Implement destination coordinates in Player class
+      m_zDst(player->getZ())  // TODO: Implement destination coordinates in Player class
+{
+    if (!player)
+    {
+        throw std::invalid_argument("Player cannot be null for MoveToLocation packet");
+    }
+}
+
+void MoveToLocation::write(SendablePacketBuffer& buffer)
+{
+    // Opcode is written automatically by base class
+    
+    std::cout << "[MoveToLocation] Sending movement for object ID: " << m_objectId 
+              << " from (" << m_x << ", " << m_y << ", " << m_z << ")"
+              << " to (" << m_xDst << ", " << m_yDst << ", " << m_zDst << ")" << std::endl;
+    
+    // Following L2J Mobius MoveToLocation.java structure EXACTLY
+    
+    // 1. Object ID
+    buffer.writeUInt32(m_objectId);
+    
+    // 2. Destination coordinates
+    buffer.writeUInt32(m_xDst);
+    buffer.writeUInt32(m_yDst);
+    buffer.writeUInt32(m_zDst);
+    
+    // 3. Current coordinates
+    buffer.writeUInt32(m_x);
+    buffer.writeUInt32(m_y);
+    buffer.writeUInt32(m_z);
+}
+
+size_t MoveToLocation::getSize() const
+{
+    // Packet ID (1) + objectId (4) + xDst (4) + yDst (4) + zDst (4) + x (4) + y (4) + z (4) = 29 bytes
+    return 29;
+} 
